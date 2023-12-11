d56cf421b1afb5819891c4a922fab6e7d1d0ebac
hyot.ahn <hyot.ahn@kakaoenterprise.com>
2023-12-11 17:07:33 +0900

    파일 압축

--- etc/mysqlTest2.py
+++ etc/mysqlTest2.py
@@ -1,5 +1,7 @@
 import subprocess
 import datetime
+import gzip
+import os
 
 profile = 'local'
 
@@ -26,13 +28,32 @@ else:
     backup_path = '/hanmail/working/hyot/backup'
 
 try:
-    for db in db_target:
-        backup_file = f"{backup_path}/{db}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.sql"
-        result = subprocess.run(["mysqldump", "--no-tablespaces", f"--host={db_host}", f"--port={db_port}", f"--user={db_user}",
-                                 f"--password={db_password}", db, f"--result-file={backup_file}"])
-        print(f"처리 결과: {result}")
-        print(f"{db} 백업이 완료되었습니다.")
-except Exception as e:
-    print(e)
+    today = datetime.datetime.now().strftime('%Y%m%d')
+    backup_path = f"{backup_path}/{today}"
+    os.mkdir(backup_path)
+except FileExistsError as e:
+    pass
 finally:
-    print("DB 백업 스크립트를 종료합니다.")
+    try:
+        for db in db_target:
+            backup_file_name = f"{backup_path}/{db}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}"
+            backup_ori_file = backup_file_name + '.sql'
+            backup_gzip_file = backup_ori_file + '.gz'
+
+            result = subprocess.run(
+                ["mysqldump", "--no-tablespaces", f"--host={db_host}", f"--port={db_port}", f"--user={db_user}",
+                 f"--password={db_password}", db, f"--result-file={backup_ori_file}"])
+
+            with open(backup_ori_file, 'r') as f_ori:
+                data = f_ori.read()
+                with gzip.open(backup_gzip_file, 'wb') as f_gzip:
+                    f_gzip.write(data.encode("utf-8"))
+
+            os.remove(backup_ori_file)
+
+            print(f"처리 결과: {result}")
+            print(f"{db} 백업이 완료되었습니다.")
+    except Exception as e:
+        print(e)
+    finally:
+        print("DB 백업 스크립트를 종료합니다.")
